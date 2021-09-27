10a8b31b3b101f4642e3c555a95878506dda62f3
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-09-27 10:39:15 +0300

    Add "Unversioned Files" of credit-score-system module

---
+++ credit-score-system/src/main/java/dev/patika/creditscoresystem/CreditScoreSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.creditscoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditScoreSystemApplication.class, args);
+    }
+
+}
---
+++ credit-score-system/src/test/java/dev/patika/creditscoresystem/CreditScoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.creditscoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
